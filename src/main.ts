67a9333f7faf7289a5f298b13e2a721359eecdad
NafisatB <nafisatbabamusa@outlook.com>
2026-08-28 20:37:54 +0100

    swagger doc to api/docs

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, document, {
+  SwaggerModule.setup('api/docs', app, document, {
     swaggerOptions: {
       persistAuthorization: true
     }
