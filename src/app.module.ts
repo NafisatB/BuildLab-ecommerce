a2f171a9edcbcb67277caebdb1a180dc89dde7eb
NafisatB <nafisatbabamusa@outlook.com>
2026-08-28 19:58:47 +0100

    configure API validation and Swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true,}),
+    ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaExceptionFilter } from './common/filters/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: false
+      }
+    })
+  );
+
+  app.useGlobalFilters(
+    new PrismaExceptionFilter()
+  )
+
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('E-Commerce Product Management API')
+  .setDescription('RESTful API for managing products in an e-commerce platform')
+  .setVersion('1.0')
+  .addTag('Products')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true
+    }
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
